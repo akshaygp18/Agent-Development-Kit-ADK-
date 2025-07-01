c9a8647299d6fdf926263d05c2d5a0113d588f02
Akshay G P <109408591+akshaygp18@users.noreply.github.com>
2025-07-01 06:36:11 +0000

    init and agent file added

---
+++ Basic_Agent/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import Agent 
+
+root_agent = Agent(
+    name = "Greeting Agent",
+    model = "gemini-2.0-flash",
+    description = "Greeting agent",
+    instruction = """
+    you are helpful assistant that greets the users.
+    Ask for the users name and greet them by name.
+    """
+
+)
